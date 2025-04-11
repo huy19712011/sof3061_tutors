7e7af965281db0c683dd50521093ad394c7f773f
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-04-11 17:18:49 +0700

    Security: configure Basic Authentication

---
+++ src/main/java/org/example/sof3061_tutors/config/SpringSecurityConfig.java
@@ -0,0 +1,24 @@
+package org.example.sof3061_tutors.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrfConfigurer -> csrfConfigurer.disable())
+                .authorizeHttpRequests(authorize -> {
+                    authorize.anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}

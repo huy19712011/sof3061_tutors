f763cfaad9e90c33076afd3cb7eb5b116c71f675
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2025-04-11 17:46:03 +0700

    Security: User and Role

---
+++ src/main/java/org/example/sof3061_tutors/entity/Role.java
@@ -0,0 +1,28 @@
+package org.example.sof3061_tutors.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+}
